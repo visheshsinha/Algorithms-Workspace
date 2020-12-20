8eb05918f363751f69a00f292485886be9ddc994
visheshsinha <visheshsinha801@gmail.com>
2020-12-20 14:19:00 +0530

    Augmented MinBST

--- python_ds/bst/bst.py
+++ python_ds/bst/bst.py
@@ -1,17 +1,44 @@
 class BSTNode(object):
-    """Node Unit in a BST """
-
     def __init__(self, parent, k):
-        """k: The key of the node."""
-
         self.key = k
         self.parent = parent
-        self.right = None
         self.left = None
+        self.right = None
+  
+    def _str(self):
+        """Internal method for ASCII art."""
+        label = str(self.key)
+        if self.left is None:
+            left_lines, left_pos, left_width = [], 0, 0
+        else:
+            left_lines, left_pos, left_width = self.left._str()
+        if self.right is None:
+            right_lines, right_pos, right_width = [], 0, 0
+        else:
+            right_lines, right_pos, right_width = self.right._str()
+        middle = max(right_pos + left_width - left_pos + 1, len(label), 2)
+        pos = left_pos + middle // 2
+        width = left_pos + middle + right_width - right_pos
+        while len(left_lines) < len(right_lines):
+            left_lines.append(' ' * left_width)
+        while len(right_lines) < len(left_lines):
+            right_lines.append(' ' * right_width)
+        if (middle - len(label)) % 2 == 1 and self.parent is not None and \
+           self is self.parent.left and len(label) < middle:
+            label += '.'
+        label = label.center(middle, '.')
+        if label[0] == '.': label = ' ' + label[1:]
+        if label[-1] == '.': label = label[:-1] + ' '
+        lines = [' ' * left_pos + label + ' ' * (right_width - right_pos),
+                 ' ' * left_pos + '/' + ' ' * (middle-2) +
+                 '\\' + ' ' * (right_width - right_pos)] + \
+          [left_line + ' ' * (width - left_width - right_width) + right_line
+           for left_line, right_line in zip(left_lines, right_lines)]
+        return lines, pos, width
+    def __str__(self):
+        return '\n'.join(self._str()[0])
 
     def find(self, k):
-        """Return the node for key k if is in the tree, or None otherwise."""
-
         if k == self.key:
             return self
         elif k < self.key:
@@ -20,32 +47,26 @@ class BSTNode(object):
             else:
                 return self.left.find(k)
         else:
-            if self.right is None:
+            if self.right is None:  
                 return None
             else:
                 return self.right.find(k)
-
+    
     def find_min(self):
         current = self
         while current.left is not None:
             current = current.left
         return current
-    
+       
     def next_larger(self):
-
-        # Has a right Subtree 
         if self.right is not None:
             return self.right.find_min()
-        
-        # Doesn't have a right Subtree
         current = self
         while current.parent is not None and current is current.parent.right:
             current = current.parent
         return current.parent
 
     def insert(self, node):
-        """Inserts a node into the subtree rooted at this node."""
-        
         if node is None:
             return
         if node.key < self.key:
@@ -60,51 +81,120 @@ class BSTNode(object):
                 self.right = node
             else:
                 self.right.insert(node)
-
+  
     def delete(self):
-
-        # When x has atmost 1 child
         if self.left is None or self.right is None:
             if self is self.parent.left:
                 self.parent.left = self.left or self.right
-                if self.parent.left is None:
+                if self.parent.left is not None:
                     self.parent.left.parent = self.parent
             else:
                 self.parent.right = self.left or self.right
-                if self.parent.right is None:
+                if self.parent.right is not None:
                     self.parent.right.parent = self.parent
             return self
+        else:
+            s = self.next_larger()
+            self.key, s.key = s.key, self.key
+            return s.delete()
+    
+    def check_ri(self):
+        if self.left is not None:
+            if self.left.key > self.key:
+                raise RuntimeError("BST RI violated by a left node key")
+            if self.left.parent is not self:
+                raise RuntimeError("BST RI violated by a left node parent "
+                                   "pointer")
+            self.left.check_ri()
+        if self.right is not None:
+            if self.right.key < self.key:
+                raise RuntimeError("BST RI violated by a right node key")
+            if self.right.parent is not self:
+                raise RuntimeError("BST RI violated by a right node parent "
+                                   "pointer")
+            self.right.check_ri()
 
-        # When x has 2 children
+class MinBSTNode(BSTNode):
+
+    def __init__(self, parent, key):
+        super(MinBSTNode, self).__init__(parent, key)
+        self.min = self
+  
+    def find_min(self):
+        return self.min
+
+    def insert(self, node):
+        if node is None:
+            return
+        if node.key < self.key:
+            if node.key < self.min.key:
+                self.min = node
+            if self.left is None:
+                node.parent = self
+                self.left = node
+            else:
+                self.left.insert(node)
+        else:
+            if self.right is None:
+                node.parent = self
+                self.right = node
+            else:
+                self.right.insert(node)
+  
+    def delete(self):
+        if self.left is None or self.right is None:
+            if self is self.parent.left:
+                self.parent.left = self.left or self.right
+                if self.parent.left is not None:
+                    self.parent.left.parent = self.parent
+                    self.parent.min = self.parent.left.min
+                else: 
+                    self.parent.min = self.parent
+                c  = self.parent
+                while c.parent is not None and c is c.parent.left:
+                    c.parent.min = c.min
+                    c = c.parent
+            else:
+                self.parent.right = self.left or self.right
+                if self.parent.right is not None:
+                    self.parent.right.parent = self.parent
+            return self
         else:
             s = self.next_larger()
             self.key, s.key = s.key, self.key
             return s.delete()
 
 class BST(object):
-    
-    def __init__(self):
+
+    def __init__(self, klass = BSTNode):
         self.root = None
-    
+        self.klass = klass
+        
+    def __str__(self):
+        if self.root is None: return '<empty tree>'
+        return str(self.root)
+
     def find(self, k):
         return self.root and self.root.find(k)
-    
-    def find_min(self):
+                
+    def find_min(self):     
         return self.root and self.root.find_min()
-
+    
     def insert(self, k):
-        node = BSTNode(None, k)
+        node = self.klass(None, k)
         if self.root is None:
+            # The root's parent is None.
             self.root = node
         else:
             self.root.insert(node)
-    
+        return node
+            
     def delete(self, k):
         node = self.find(k)
         if node is None:
             return None
         if node is self.root:
-            pseudoroot = BSTNode(None, 0)
+            pseudoroot = self.klass(None, 0)
             pseudoroot.left = self.root
             self.root.parent = pseudoroot
             deleted = self.root.delete()
@@ -113,47 +203,25 @@ class BST(object):
                 self.root.parent = None
             return deleted
         else:
-            return node.delete()
-    
+            return node.delete()   
+        
     def next_larger(self, k):
         node = self.find(k)
         return node and node.next_larger()
+    
+    def check_ri(self):
+        if self.root is not None:
+            if self.root.parent is not None:
+                raise RuntimeError("BST RI violated by the root node's parent " 
+                                   "pointer.")
+            self.root.check_ri()
+    
 
-    def __str__(self):
-        if self.root is None: return '<empty tree>'
-        def recurse(node):
-            if node is None: return [], 0, 0
-            label = str(node.key)
-            left_lines, left_pos, left_width = recurse(node.left)
-            right_lines, right_pos, right_width = recurse(node.right)
-            middle = max(right_pos + left_width - left_pos + 1, len(label), 2)
-            pos = left_pos + middle // 2
-            width = left_pos + middle + right_width - right_pos
-            while len(left_lines) < len(right_lines):
-                left_lines.append(' ' * left_width)
-            while len(right_lines) < len(left_lines):
-                right_lines.append(' ' * right_width)
-            if (middle - len(label)) % 2 == 1 and node.parent is not None and \
-               node is node.parent.left and len(label) < middle:
-                label += '.'
-            label = label.center(middle, '.')
-            if label[0] == '.': label = ' ' + label[1:]
-            if label[-1] == '.': label = label[:-1] + ' '
-            lines = [' ' * left_pos + label + ' ' * (right_width - right_pos),
-                     ' ' * left_pos + '/' + ' ' * (middle-2) +
-                     '\\' + ' ' * (right_width - right_pos)] + \
-              [left_line + ' ' * (width - left_width - right_width) +
-               right_line
-               for left_line, right_line in zip(left_lines, right_lines)]
-            return lines, pos, width
-        return '\n'.join(recurse(self.root) [0])
+class MinBST(BST):
+    """An augmented BST that keeps track of the node with the minimum key."""
+    def __init__(self):
+        super(MinBST, self).__init__(MinBSTNode)
 
-
-
-class MinBSTNode(BSTNode):
-    def __init__(self, parent, k):
-        super(MinBSTNode, self).__init__(parent, k)
-        self.min = self
 
 def test(args=None, BSTtype=BST):
     import random, sys
